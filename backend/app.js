be724a94041a13b040bf970fc74b2c317b8774c5
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2021-06-27 07:15:24 -0700

    create routers

--- backend/app.js
+++ backend/app.js
@@ -4,43 +4,18 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 require('dotenv/config');
-
 const api = process.env.API_URL;
+
+
+const productsRouter = require('./routers/products')
+
 
 //Middleware
 app.use(express.json());
 app.use(morgan('tiny'));
 
-
-const Product = require('./models/product');
-
-app.get(`${api}/products`, async (req, res)=>{
-    const productList = await Product.find();
-
-    if(!productList){
-        res.status(500).json({success: false})
-    }
-    res.send(productList);
-})
-
-app.post(`${api}/products`, (req, res)=>{
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-    
-})
-
+//Routers
+app.use(`${api}/products`, productsRouter)
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
