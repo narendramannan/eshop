39e8e21e07ce7179aec59692196c698e5c90fa36
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2021-06-27 06:15:40 -0700

    created backend using express

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+//Middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+
+const Product = require('./models/product');
+
+app.get(`${api}/products`, async (req, res)=>{
+    const productList = await Product.find();
+
+    if(!productList){
+        res.status(500).json({success: false})
+    }
+    res.send(productList);
+})
+
+app.post(`${api}/products`, (req, res)=>{
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+    
+})
+
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database connection is ready')
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000, ()=>{
+    
+    console.log('server is running http://localhost:3000');
+})
