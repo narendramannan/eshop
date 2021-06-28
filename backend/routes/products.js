da55886ff73e65305d43bdf871ba6fdb2b65e5aa
Narendra Mannan <53171217+narendramannan@users.noreply.github.com>
2021-06-28 00:14:38 -0700

    update product and category route with post get delete

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,31 +1,54 @@
 const {Product} = require('../models/product');
 const express = require('express');
+const { Category } = require('../models/category');
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
-    const productList = await Product.find();
+    const productList = await Product.find().select('name image -_id').populate('category');
 
     if(!productList) {
         res.status(500).json({success: false})
     } 
     res.send(productList);
 })
+router.get(`/:id`, async (req, res) =>{
+    const product = await Product.findById(req.params.id).populate('category');
 
-router.post(`/`, (req, res) =>{
+    if(!product) {
+        res.status(500).json({success: false})
+    } 
+    res.send(product);
+})
+
+router.post(`/`, async (req, res) =>{
+
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category!')
+
     const product = new Product({
         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
         image: req.body.image,
-        countInStock: req.body.countInStock
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
     })
 
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+    product = await product.save();
+
+    if(!product){
+        return res.status(500).send('The product cannot be created!')
+    }
+    
+
+    res.send(product);
+
+    
 })
 
 module.exports =router;
